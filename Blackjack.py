8ae43b8ddfc539a69206ced2d4f89cb6c756e9f7
hutch2323 <marcus.hutchings30@gmail.com>
2021-07-02 10:45:11 -0230

    added card.py (card functions), bet validation into db.py, logic updates

--- Blackjack.py
+++ Blackjack.py
@@ -1,56 +1,5 @@
-import random
+import card
 import db
-
-def createDeck():
-    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-    pointValues = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    deck = []
-
-    cardCounter = 0
-    for i in range(len(ranks)):
-        for j in range(len(suits)):
-            card = [suits[j], ranks[i], pointValues[i]]
-            deck.append(card)
-            #deck[cardCounter][0] = suits[j]
-            #deck[cardCounter][1] = ranks[i]
-            #deck[cardCounter][2] = pointValues[i]
-            cardCounter += 1
-    return deck
-
-
-def dealCard(deck):
-    cardToDeal = random.choice(deck)
-    deck.remove(cardToDeal)
-    return cardToDeal
-
-
-def checkForAce(card, user, score):
-    if (card[1] == "Ace") and (score <= 10):
-        if (user == "player"):
-            aceChoice = int(input("\nYou were dealt an ace. 1 or 11?: "))
-            if (aceChoice == 1):
-                card[2] = 1
-            elif (aceChoice == 11):
-                card[2] = 11
-        elif (user == "dealer"):
-            card[2] = 11
-
-def processCardDeal(hand, deckOfCards, user, points):
-    dealtCard = dealCard(deckOfCards)
-    checkForAce(dealtCard, user, points)
-    points += dealtCard[2]
-    hand.append(dealtCard)
-    return points
-
-def showHand(hand, user):
-    if(user == "player"):
-        print("\nYOUR CARDS")
-    elif(user == "dealer"):
-        print("\nDEALER'S CARDS")
-
-    for card in hand:
-        print(card[1] + " of " + card[0])
 
 def displayTitle():
     print("BLACKJACK!")
@@ -59,19 +8,35 @@ def displayTitle():
 def main():
 
     playAgain ="y"
+    newBet = "y"
+    addFunds = "y"
+    isValidBet = True
     
     while(playAgain == "y"):
         displayTitle()
-        fileName = "money.txt"
         
-        money = db.importFile(fileName)
-
+        money = db.importFile()
+        if (money < 5):
+            addFunds = input("\nYour account currently has less than the required $5 to play. Would you like to add additional funds (y/n)?: ")
+            if (addFunds.lower() == "y"):
+                money = db.addMoney(money)
+            else:
+                break
+            
         print("\nMoney: " + str(money))
         betAmount = float(input("Bet amount: "))
 
-        money = db.processBet(money, betAmount, fileName)
+        isValidBet = db.validateBet(money, betAmount)
+
+        if (isValidBet == False):
+            print()
+            break
+        else:
+            money = db.refreshFunds()
         
-        deckOfCards = createDeck()
+        money = db.processBet(money, betAmount)
+        
+        deckOfCards = card.createDeck()
 
         playerPoints = 0
         dealerPoints = 0
@@ -79,29 +44,29 @@ def main():
         dealerHand = []
 
         user = "player"
-        playerPoints = processCardDeal(playerHand, deckOfCards, user, playerPoints)
+        playerPoints = card.processCardDeal(playerHand, deckOfCards, user, playerPoints)
         
         user = "dealer"
-        dealerPoints = processCardDeal(dealerHand, deckOfCards, user, dealerPoints)
+        dealerPoints = card.processCardDeal(dealerHand, deckOfCards, user, dealerPoints)
 
         print("\nDEALER'S SHOW CARD:")
         print(dealerHand[0][1] + " of " + dealerHand[0][0])
 
         user = "player"
-        playerPoints = processCardDeal(playerHand, deckOfCards, user, playerPoints)
+        playerPoints = card.processCardDeal(playerHand, deckOfCards, user, playerPoints)
 
         user = "dealer"
-        dealerPoints = processCardDeal(dealerHand, deckOfCards, user, dealerPoints)
+        dealerPoints = card.processCardDeal(dealerHand, deckOfCards, user, dealerPoints)
         
         user = "player"
-        showHand(playerHand, user)
+        card.showHand(playerHand, user)
         hitOrStand = input("\nHit or stand? (hit/stand): ")
         
         #while loop to handle player's turn
         while (playerPoints <= 21) and (hitOrStand.lower() == "hit"):
-            playerPoints = processCardDeal(playerHand, deckOfCards, user, playerPoints)
+            playerPoints = card.processCardDeal(playerHand, deckOfCards, user, playerPoints)
                 
-            showHand(playerHand, user)
+            card.showHand(playerHand, user)
 
             if (playerPoints >= 21):
                 break
@@ -110,10 +75,10 @@ def main():
 
         user = "dealer"
         while((dealerPoints <= 17) and (playerPoints <= 21)):
-            dealerPoints = processCardDeal(dealerHand, deckOfCards, user, dealerPoints)
+            dealerPoints = card.processCardDeal(dealerHand, deckOfCards, user, dealerPoints)
 
 
-        showHand(dealerHand, user)
+        card.showHand(dealerHand, user)
                 
         print("\nYOUR POINTS:     " + str(playerPoints))
         print("DEALER'S POINTS: " + str(dealerPoints))
@@ -121,34 +86,20 @@ def main():
         if (playerPoints <=21):
             if((playerPoints > dealerPoints) or (dealerPoints > 21)):
                 print("\nYou win.")
-                money = db.processWin(money, betAmount, fileName)
+                money = db.processWin(money, betAmount)
             elif(playerPoints == dealerPoints):
                 print("\nIt's a draw!")
-                money = db.processTie(money, betAmount, fileName)
+                money = db.processTie(money, betAmount)
             elif (dealerPoints > playerPoints):
                 print("\nSorry. You lose.")
         else:
             print("\nSorry. You lose.")
 
-#        if (playerPoints <= 21) and (dealerPoints <= 21):
-#            if (playerPoints > dealerPoints):
-#                print("\nYou win.")
-#                money = db.processWin(money, betAmount, fileName)
-#            elif (playerPoints == dealerPoints):
-#                print("\nIt's a draw.")
-#            else:
-#                print("\nYou lose.")
-#        elif (playerPoints <= 21) and (dealerPoints > 21):
-#            print("\nYou win.")
-#            money = db.processWin(money, betAmount, fileName)
-#        else:
-#            print("\nYou lose.")
-
         print("Money: " + str(money))
 
         playAgain = input("\nPlay again? (y/n): ")
         print()
-
+        
     print("Come back soon!")
     print("Bye!")
 
