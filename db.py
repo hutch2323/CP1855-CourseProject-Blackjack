200c533a7518284301a42427d3d0e7a5202b54a2
hutch2323 <marcus.hutchings30@gmail.com>
2021-07-07 23:37:36 -0230

    Updates 07-07-21

--- db.py
+++ db.py
@@ -1,15 +1,48 @@
 import sys
 
+"""
+This module handles all the financial calculations for the blackjack game. It also reads a monetary amount from a file, updates the amount, and saves the file.
+"""
+
+# Constant/global variable used to hold the file name used by several of the functions in the module
 FILENAME = "money.txt"
 
+
+# This function displays a message and exit the program
 def exitProgram():
     print("\nTerminating program.")
     sys.exit()
 
+
+# This function retrieves a bet amount from the user. It also displays the curent balance (money) parameter while attempting to place a bet. It will return the user's bet amount.
+def getUserBet(money):
+    while True:
+        print("\nMoney: " + str(money))
+        try:
+            betAmount = float(input("Bet amount: "))
+        except ValueError:
+            print("Invalid number. Please try again!")
+            continue
+        except Exception as e:
+            print("Unexpected error has occured!")
+            print(type(e), e)
+            exitProgram()
+        # Validation to ensure that the bet amount is >= 5 and <= 1000
+        if ((betAmount < 5) or (betAmount > 1000)):
+            print("Error, invalid bet amount. Please select an amount between 5 and 1000.")
+            continue
+        else:
+            break
+
+    return betAmount
+
+
+# This function checks to see if the user's balance (money parameter) is greater than or equal to 5. If balance is less than 5, user prompted to add additional funds to account.
 def checkForMinimumBalance(money):
     while (money < 5):
         addFunds = input("\nYour account ($" + str(money) + ") currently has less than the required $5 to play.\n" +
                              "Would you like to add additional funds (y/n)?: ")
+        # If the user wants to add more money to their account, call the addMoney() function. Otherwise, return False.
         if (addFunds.lower() == "y"):
             money = addMoney(money)
             continue
@@ -17,8 +50,11 @@ def checkForMinimumBalance(money):
             print()
             return False
 
+    # If the minimum balance is achieved, return True
     return True
 
+
+# Function that takes the current balance (money) as a parameter and updates the file
 def saveFile(money):
     try:
         with open(FILENAME, "w") as file:
@@ -32,9 +68,13 @@ def saveFile(money):
         print(type(e), e)
         exitProgram()
 
+
+# Function used to read the text file and assign the balance value the money variable. This variable is then returned.
 def importFile():
+    # Initialize money variable to 0
     money = 0
 
+    # If the file isn't found or there is an unexpected error, a message will be displayed and the exitProgram() function will be called
     try:
         with open(FILENAME) as file:
             money = file.readline()
@@ -48,25 +88,36 @@ def importFile():
 
     return float(money)
 
+
+# Calls importFile() function and returns the money value
 def updateBalance():
     money = importFile()
     return money
 
+
+# Function that processes the user's bet. Takes the current balance and betAmount as parameters, removes the bet amount, then updates the file holding the balance
 def processBet(money, betAmount):
     money -= betAmount
     saveFile(money)
-    
+
+
+# Function that processes a user win. Takes current balance and bet amount as parameters, and adds the betAmount * 3/2 (betting odds) to the balance. Then calls saveFile()
 def processWin(money, betAmount):
     money += round(betAmount * (3/2), 2)
     saveFile(money)
     return money
 
+
+# Function that processes a tie. Takes current balance and bet amount as parameters, and adds (returns) the bet amount back to the user's balance (money). Saves the file and returns the updated balance
 def processTie(money, betAmount):
     money += betAmount
     saveFile(money)
     return money
 
+
+# Function used to handle user adding money to their account balance. Takes the current balance as a parameter, adds the requsted amount, saves the file, and returns new balance (money)
 def addMoney(money):
+    # Prompts user for an amount to add and validates the value
     while True:
         try:
             amountToAdd = float(input("\nAmount to add: "))
@@ -82,70 +133,71 @@ def addMoney(money):
         else:
             print("Please enter a valid number greater than zero. Try again!")
 
+    # Add funds to current balance (money)
     money += amountToAdd
     saveFile(money)
     return money
 
-def validateBet(money, betAmount):
-    newBet = "y"
-    addFunds = "y"
-    
-    while ((betAmount < 5) or (betAmount > 1000)):
-        print("\nError, invalid bet amount. Please select an amount between 5 and 1000.")
-        print("\nMoney: " + str(money))
-        try:
-            betAmount = float(input("Bet amount: "))
-        except ValueError:
-            print("Invalid number. Please try again!\n")
-            continue
-        except Exception as e:
-            print("An unexpected error has occured")
-            print(type(e), e)
-            exitProgram()
 
-    while (betAmount > money):   
-        addFunds = input("\nError. Your account ($" + str(money) + ") currently has less funds than the bet amount.\n" +
-                         "Would you like to add additional funds (y/n)?: ")
-        if (addFunds.lower() == "y"):
-                money = addMoney(money)
-        else:
-            newBet = input("\nWould you like to make a different bet (y/n)?: ")
-            if (newBet.lower() == "y"):
-                while True:
-                    try:
-                        print("\nMoney: " + str(money))
-                        betAmount = float(input("Bet amount: "))
-                    except ValueError:
-                        print("Invalid number. Please try again!\n")
-                        continue
-                    except Exception as e:
-                        print("An unexpected error has occured")
-                        print(type(e), e)
-                        exitProgram()
-                    if ((betAmount < 5) or (betAmount > 1000)):
-                        print("Error, invalid bet amount. Please select an amount between 5 and 1000.")
+def changeBetAmount(money):
+    pass
+    
+
+# Function used to validate the user's bet. Takes the balance (money) and the betAmount as parameters. Will return the bet amount (0 if the bet is cancelled)
+def validateBet(money, betAmount):
+    # Initialize the options of adding funds or making a new bet to "y"
+    newBet = "n"
+    addFunds = "n"
+
+    while True:
+        if (money >= betAmount):
+            return betAmount
+
+        # While loop used to check if the bet requested exceeds the account balance (money)
+        while (betAmount > money):   
+            addFunds = input("\nError. Your account ($" + str(money) + ") currently has less funds than the bet amount.\n" +
+                             "Would you like to add additional funds (y/n)?: ")
+            # If bet amount exceeds money available, ask user if they would like to add funds to their account
+            if (addFunds.lower() == "y"):
+                    # Call addMoney() function and update money variable with new balance
+                    money = addMoney(money)                
+            else:
+                # If user doesn't want to add more money, ask if they would like to place a different bet
+                newBet = input("\nWould you like to make a different bet (y/n)?: ")
+                if (newBet.lower() == "y"):
+                    # While loop to handle bet validation
+                    #while True:
+                        # Call getUserBet() function and assign returned value to betAmount variable
+                    betAmount = getUserBet(money)
+                  
+                    continue
+                else:
+                    betAmount = 0
+                    return betAmount
+
+
+            if(addFunds.lower() == "y") and (newBet.lower() != "y"):
+                confirmBet = "n"
+                print("\nMoney: " + str(money))
+                print("Bet Amount: " + str(betAmount))
+                confirmBet = input("Confirm bet (y/n)?: ")
+
+                if (confirmBet == "y"):
+                    return betAmount
+                else:
+                    # If user doesn't want to add more money, ask if they would like to place a different bet
+                    newBet = input("\nWould you like to make a different bet (y/n)?: ")
+                    if (newBet.lower() == "y"):
+                        # Call getUserBet() function and assign returned value to betAmount variable
+                        betAmount = getUserBet(money)
+                      
                         continue
                     else:
-                        break
-            else:
-                break
+                        betAmount = 0
+                        return betAmount
 
-    if ((newBet.lower() != "y") and (addFunds.lower() != "y") and (betAmount > money)):
-        return False
-    else:
-        processBet(money, betAmount)
-        return True
-
+        # If the user's bet is more than the current balance (money), user declines to add additional funds, and declines to place a new bet, return False
+        if ((newBet.lower() != "y") and (addFunds.lower() != "y") and (betAmount > money)):
+            betAmount = 0
+            return betAmount
     
-
-#def saveFile(money, fileName):
-#    with open(fileName, "w") as file:
-#        for item in money:
-#            file.write(item +"\n")
-            
-
-#def importFile(money, fileName):
-#    with open(fileName) as file:
-#        for line in file:
-#            line = line.replace("\n", "")
-#            money.append(line)
